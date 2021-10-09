3b1f45f46ed0dbb6cc8254fac4e279da299d2631
Andrew <nester.letopisets@gmail.com>
2021-10-09 15:07:11 +0300

    header, vars(2.0), global(2.0), js code to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(function () {
+    const burger = document.querySelector('.menu__burger');
+    const menu = document.querySelector('.menu');
+    const menuLink = document.querySelectorAll('.menu__link');
+    const body = document.querySelector('body');
 
+    burger.onclick = function () {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+        body.classList.toggle('active');
+    };
+
+    menuLink.forEach(function (item) {
+        item.onclick = function () {
+            burger.classList.remove('active');
+            menu.classList.remove('active');
+            body.classList.remove('active');
+        };
+    })
 })
